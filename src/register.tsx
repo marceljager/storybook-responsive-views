dd1c8a1f118b701b50ca8d4606ee7f3f6ecb874f
Marcel Jäger <mjager9@gmail.com>
2020-05-07 13:17:24 +0200

    fix register default export

--- src/register.tsx
+++ src/register.tsx
@@ -3,11 +3,13 @@ import addons, { types } from '@storybook/addons'
 
 import Tool from './Tool'
 
-addons.register('responsive-addon', () => {
-  addons.addPanel('responsive-addon/panel', {
-    title: 'Responsive Views',
-    type: types.TOOL,
-    match: ({ viewMode }) => viewMode === 'story',
-    render: () => <Tool />,
+export default () => {
+  addons.register('responsive-addon', () => {
+    addons.addPanel('responsive-addon/panel', {
+      title: 'Responsive Views',
+      type: types.TOOL,
+      match: ({ viewMode }) => viewMode === 'story',
+      render: () => <Tool />,
+    });
   });
-});
+}
